fb735085545e7224cb00503d516f43cce7feb8cb
samantha-nickerson <nick0046@algonquinlive.com>
2014-11-19 13:24:54 -0500

    Added work section
    Added section displaying my work.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,23 @@
 var $win = $(window);
-var $sun = $('.sun');
-var $sunSection = $('.sun-section');
-var $dipper = $('.dipper');
-var $dipperSection = $('.dipper-section');
-var $shuttle = $('.shuttle');
-var $shuttleSection = $('.shuttle-section');
-var $ships = $('.ships img');
-var $shipsSection = $('.ships-section');
-var shipsSectionOffset = $shipsSection.offset();
+var $intro = $('.intro');
+var $introSection = $('.intro-section');
+var $icons = $('.icons img');
+var $iconsSection = $('.icons-section');
+var iconsSectionOffset = $iconsSection.offset();
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
-	var shipsPos = (scrollPos - shipsSectionOffset.top) / 2;
+	var iconsPos = (scrollPos - iconsSectionOffset.top) / 2;
 
-	$sun.css('transform', 'rotate(-' + scrollPos / 5 + 'deg)');
-	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
+	$introSection.css('background-position', 'center ' + scrollPos / 1 + 'px');
 
-	$shipsSection.css('background-position', 'center ' + shipsPos + 'px');
+	$iconsSection.css('background-position', 'center ' + iconsPos + 'px');
 });
 
-$dipperSection.waypoint(function (direction) {
+$iconsSection.waypoint(function(direction) {
 	if (direction == 'down') {
-		$dipper.addClass('js-dipper-animate');
-	} else {
-		$dipper.removeClass('js-dipper-animate');
-	}
-}, { offset: '50%' });
-
-$shuttleSection.waypoint(function (direction) {
-	if (direction == 'down') {
-		$shuttle.addClass('js-shuttle-animate');
-	} else {
-		$shuttle.removeClass('js-shuttle-animate');
-	}
-}, { offset: '50%' });
-
-$shipsSection.waypoint(function(direction) {
-	if (direction == 'down') {
-		$ships.addClass('js-ships-animate');
+		$icons.addClass('js-icons-animate');
 	}else {
-		$ships.removeClass('js-ships-animate');
+		$icons.removeClass('js-icons-animate');
 	}
 }, { offset: '60%' });
