560e31260d68534f28fe564cc31cb476e3b0e3bb
samantha-nickerson <nick0046@algonquinlive.com>
2014-12-05 17:02:40 -0500

    Made adjustments to the design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
-var $win = $(window);
-var $intro = $('.intro');
-var $introSection = $('.intro-section');
 var $icons = $('.icons img');
 var $services = $('.services');
-var servicesOffset = $services.offset();
-
-$win.on('scroll', function () {
-	var scrollPos = $win.scrollTop();
-	var iconsPos = (scrollPos - servicesOffset.top) / 2;
-
-	$introSection.css('background-position', 'center ' + scrollPos / 1 + 'px');
-
-	$services.css('background-position', 'center ' + iconsPos + 'px');
-});
 
 $services.waypoint(function(direction) {
 	if (direction == 'down') {
